c55a64a62db54a10b823d33c79326b28927d2efb
Rolincero <ms.mayron29@gmail.com>
2022-12-18 10:22:03 +1000

    Formatted code for easy reading

--- Homework_1/Excersise 2.cs
+++ Homework_1/Excersise 2.cs
@@ -11,12 +11,16 @@ namespace Homework_1
         public static void FindMax()
         {
             Console.WriteLine("Программа нахождения максимального из двух.");
+
             Console.Write("Введите первое число: ");
             int a = int.Parse(Console.ReadLine());
+
             Console.Write("Введите второе число: ");
             int b = int.Parse(Console.ReadLine());
+
             if (a < b) Console.WriteLine($"Число {b} большее.");
             else Console.WriteLine($"Число {a} большее.");
+
             Console.WriteLine("Для продолжения нажмите любую клавишу...");
             Console.ReadKey();
         }
--- Homework_1/Excersise 4.cs
+++ Homework_1/Excersise 4.cs
@@ -12,14 +12,19 @@ namespace Homework_1
         {
             Console.Clear();
             Console.WriteLine("Программа нахождения максимального из трех чисел.");
+
             Console.Write("Введите первое число: ");
             int a = int.Parse(Console.ReadLine());
+
             Console.Write("Введите второе число: ");
             int b = int.Parse(Console.ReadLine());
+
             Console.Write("Введите третье число: ");
             int c = int.Parse(Console.ReadLine());
+
             int[] ints = {a, b, c};
             Console.WriteLine($"{ints.Max()} максимальное.");
+
             Console.WriteLine("Для продолжения нажмите любую клавишу...");
         }
     }
