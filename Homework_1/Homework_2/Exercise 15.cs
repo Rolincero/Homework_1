a26cab5baa79d000b75ad40d0941524fd37b6629
Rolincero <ms.mayron29@gmail.com>
2022-12-18 18:40:34 +1000

    Exercise 15 ready

--- Homework_1/Homework_2/Exercise 15.cs
+++ Homework_1/Homework_2/Exercise 15.cs
@@ -8,5 +8,38 @@ namespace Homework_1.Homework_2
 {
     internal class Exercise_15
     {
+        public static void WhatDayToday()
+        {
+            int input = int.Parse(Console.ReadLine());
+
+            if (input > 0 & input < 8)
+            {
+                switch (input)
+                {
+                    case 1:
+                        Console.WriteLine("Нет");
+                        break;
+                    case 2:
+                        Console.WriteLine("Нет");
+                        break;
+                    case 3:
+                        Console.WriteLine("Нет");
+                        break;
+                    case 4:
+                        Console.WriteLine("Нет");
+                        break;
+                    case 5:
+                        Console.WriteLine("Нет");
+                        break;
+                    case 6:
+                        Console.WriteLine("Да");
+                        break;
+                    case 7:
+                        Console.WriteLine("Да");
+                        break;
+                }
+            }
+            else Console.WriteLine("Нет такого дня.");
+        }
     }
 }
