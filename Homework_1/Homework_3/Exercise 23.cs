526a27908dd5ce1b3fc420b4cc7c288d17d16220
Rolincero <ms.mayron29@gmail.com>
2022-12-24 19:19:56 +1000

    Exercise 23 Completed

---
+++ Homework_1/Homework_3/Exercise 23.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_1.Homework_3
+{
+    internal class Exercise_23
+    {
+        public static void CubePow(int input)
+        {
+            for (double i = 0; i <= input; i++)
+            {
+                Console.Write($"{Math.Pow(i, 3)}, ");
+            }
+            Console.WriteLine("\n");
+        }
+    }
+}
--- Homework_1/Program.cs
+++ Homework_1/Program.cs
@@ -12,13 +12,9 @@ namespace Homework_1
     {
         static void Main()
         {
-            double A1 = double.Parse(Console.ReadLine());
-            double A2 = double.Parse(Console.ReadLine());
-            double A3 = double.Parse(Console.ReadLine());
-            double B1 = double.Parse(Console.ReadLine());
-            double B2 = double.Parse(Console.ReadLine());
-            double B3 = double.Parse(Console.ReadLine());
-            Console.WriteLine(Exercise_21.ThirdDimDist(A1,A2,A3,B1,B2,B3));
+            int input = int.Parse(Console.ReadLine());
+            Exercise_23.CubePow(input);
+
         }
     }
 }
