205217ef9c7d2e2d3559249cf5d9d99555390224
Rolincero <ms.mayron29@gmail.com>
2023-01-21 23:00:52 +1000

    Fixed exercise 43

--- Homework_1/Program.cs
+++ Homework_1/Program.cs
@@ -17,9 +17,18 @@ namespace Homework_1
     {
         static void Main()
         {
-            Console.Write("Введите числа через запятую, без пробелов. \nПример: 1,2,3,4 \n");
-            string input = Console.ReadLine();
-            Console.WriteLine($"Чисел больше нуля: {Exercise_41.Counter(input)}");
+            double x = 0;
+            double y = 0;
+            Console.Write("Введите значение b1: ");
+            double b1 = int.Parse(Console.ReadLine());
+            Console.Write("Введите значение k1: ");
+            double k1 = int.Parse(Console.ReadLine());
+            Console.Write("Введите значение b2: ");
+            double b2 = int.Parse(Console.ReadLine());
+            Console.Write("Введите значение k2: ");
+            double k2 = int.Parse(Console.ReadLine());
+            Exercise_43.FindCrossVector(b1, k1, b2, k2, ref x, ref y);
+            Console.WriteLine($"Линии пересекутся в точке [{Math.Round(x, 2)} ; {Math.Round(y, 2)}]");
         }
     }
 }
