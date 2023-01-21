abea812738f1817092098e206144937aceb920f5
Rolincero <ms.mayron29@gmail.com>
2023-01-21 13:25:33 +1000

    Exercise 43 Completed

--- Homework_1/Homework_6/Exercise 43.cs
+++ Homework_1/Homework_6/Exercise 43.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,10 @@ namespace Homework_1.Homework_6
 {
     internal class Exercise_43
     {
+        public static void FindCrossVector(double b1, double k1, double b2, double k2, ref double x, ref double y)
+        {
+            x = (-b2 + b1) / (-k1 + k2);
+            y = k2 * x + b2;
+        }
     }
 }
--- Homework_1/Program.cs
+++ Homework_1/Program.cs
@@ -5,6 +5,7 @@ using Homework_1.Homework_5;
 using Homework_1.Homework_6;
 using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,10 +16,18 @@ namespace Homework_1
     {
         static void Main()
         {
-            Console.Write("Введите числа через запятую, без пробелов. \nПример: 1,2,3,4 \n");
-            string input = Console.ReadLine();
-            var array = input.Split(',');
-            Console.WriteLine($"Чисел больше нуля: {Exercise_41.Counter(array)}");
+            double x = 0;
+            double y = 0;
+            Console.Write("Введите значение b1: ");
+            double b1 = int.Parse(Console.ReadLine());
+            Console.Write("Введите значение k1: ");
+            double k1 = int.Parse(Console.ReadLine());
+            Console.Write("Введите значение b2: ");
+            double b2 = int.Parse(Console.ReadLine());
+            Console.Write("Введите значение k2: ");
+            double k2 = int.Parse(Console.ReadLine());
+            Exercise_43.FindCrossVector(b1, b2, k1, k2, ref x, ref y);
+            Console.WriteLine($"Линии пересекутся в точке [{Math.Round(x,2)} ; {Math.Round(y,2)}]");
         }
     }
 }
